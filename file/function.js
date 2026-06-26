88e6120758d338175b6acb3d1cd5fa3c4394806d
JivanNikas01 <jivannikas01@gmail.com>
2026-06-26 08:03:05 +0000

    photos

--- file/function.js
+++ file/function.js
@@ -416,12 +416,24 @@
     const grid = qs('#galleryGrid');
     if(!grid) return;
     const urls = [
-      '../1.jpeg',
-      '../4.jpeg',
-      '../8.jpeg',
-      '../10.jpeg',
-      '../photos/16.jpeg',
-      '../photos/18.jpeg'
+    "photos/1.jpeg",
+    "photos/2.jpeg",
+    "photos/3.jpeg",
+    "photos/4.jpeg",
+    "photos/5.jpeg",
+    "photos/6.jpeg",
+    "photos/7.jpeg",
+    "photos/8.jpeg",
+    "photos/9.jpeg",
+    "photos/10.jpeg",
+    "photos/11.jpeg",
+    "photos/12.jpeg",
+    "photos/13.jpeg",
+    "photos/14.jpeg",
+    "photos/15.jpeg",
+    "photos/16.jpeg",
+    "photos/17.jpeg",
+    "photos/18.jpeg"
     ];
     urls.forEach((u,i) => {
       const p = document.createElement('div'); 
